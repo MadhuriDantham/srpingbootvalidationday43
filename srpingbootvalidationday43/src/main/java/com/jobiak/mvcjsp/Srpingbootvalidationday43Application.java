450b58702ffc628f038fd14796eb50cc890503ce
madhuridantham <danthammadhuri5@gmail.com>
2022-05-17 10:51:48 +0530

    done

---
+++ srpingbootvalidationday43/src/main/java/com/jobiak/mvcjsp/Srpingbootvalidationday43Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.mvcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srpingbootvalidationday43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Srpingbootvalidationday43Application.class, args);
+	}
+
+}
